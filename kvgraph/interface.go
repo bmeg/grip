a922984494b5266018412fa7dd53be65bc3231f5
Kyle Ellrott <kellrott@gmail.com>
2018-01-21 13:01:03 -0800

    Fixing the linting of the key/value driver module

--- kvgraph/interface.go
+++ kvgraph/interface.go
@@ -5,8 +5,11 @@ import (
 	"github.com/bmeg/arachne/gdbi"
 )
 
+// KVBuilder is function implemented by the various key/value storage drivers
+// that returns an initialized KVInterface given a file/path argument
 type KVBuilder func(path string) (KVInterface, error)
 
+// KVInterface is the base interface for key/value based graph driver
 type KVInterface interface {
 	HasKey(key []byte) bool
 	Set(key, value []byte) error
@@ -18,6 +21,8 @@ type KVInterface interface {
 	Close() error
 }
 
+// KVIterator is a genetic interface used by KVInterface.View to allow the
+// KVGraph to scan the values stored in the key value driver
 type KVIterator interface {
 	Seek(k []byte) error
 	Valid() bool
@@ -28,26 +33,35 @@ type KVIterator interface {
 	Get(key []byte) ([]byte, error)
 }
 
+// KVTransaction is a genetic interface used by KVInterface.Update to allow the
+// KVGraph to alter the values stored in the key value driver
 type KVTransaction interface {
 	Delete(key []byte) error
 }
 
+// KVGraph implements the ArachneInterface using a generic key/value storage driver
 type KVGraph struct {
 	kv KVInterface
 }
 
+// KVInterfaceGDB implements the GDB interface using a genertic key/value storage driver
 type KVInterfaceGDB struct {
 	kv    KVInterface
 	graph string
 }
 
-var kvMap map[string]KVBuilder = make(map[string]KVBuilder)
+var kvMap = make(map[string]KVBuilder)
 
+// AddKVDriver registers a KeyValue storage driver to the list of avalible drivers.
+// Driver list the RocksDB are only included with some build tags and aren't
+// always avalible
 func AddKVDriver(name string, builder KVBuilder) error {
 	kvMap[name] = builder
 	return nil
 }
 
+// NewKVArachne intitalize a new key value driver give the name of the
+// driver and a path/url
 func NewKVArachne(name string, path string) (gdbi.ArachneInterface, error) {
 	if x, ok := kvMap[name]; ok {
 		kv, err := x(path)
@@ -56,6 +70,7 @@ func NewKVArachne(name string, path string) (gdbi.ArachneInterface, error) {
 	return nil, fmt.Errorf("Driver %s Not Found", name)
 }
 
+// NewKVGraph creats a new instance of KVGraph given a KVInterface
 func NewKVGraph(kv KVInterface) gdbi.ArachneInterface {
 	return &KVGraph{kv: kv}
 }
