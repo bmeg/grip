2d9b082c63e1bef5336eb65e83b889c7c077af11
Kyle Ellrott <kellrott@gmail.com>
2018-02-13 13:32:58 -0800

    Fixing lint issues

--- jsonpath/jsonpath.go
+++ jsonpath/jsonpath.go
@@ -1,46 +1,44 @@
-
 package jsonpath
 
-
 import (
-  "reflect"
-  structpb "github.com/golang/protobuf/ptypes/struct"
-  "github.com/oliveagle/jsonpath"
-  "github.com/bmeg/arachne/protoutil"
+	"github.com/bmeg/arachne/protoutil"
+	structpb "github.com/golang/protobuf/ptypes/struct"
+	"github.com/oliveagle/jsonpath"
+	"reflect"
 )
 
-
+// Operator the type of comparison operation to run
 type Operator int
 
 const (
-  EQ Operator = iota
-  NE
-  //GT
-  //LT
-  //IN
+	//EQ Equal
+	EQ Operator = iota
+	//NE Not Equal
+	NE
+	//GT
+	//LT
+	//IN
 )
 
+//CompareFields compares two ProtoBuf json structs using JSONPaths
+func CompareFields(a, b *structpb.Struct, aPath, bPath string, op Operator) (bool, error) {
+	aMap := protoutil.AsMap(a)
+	bMap := protoutil.AsMap(b)
 
-func CompareFields(a, b *structpb.Struct, aPath, bPath string, op Operator ) (bool, error) {
-  aMap := protoutil.AsMap(a)
-  bMap := protoutil.AsMap(b)
-  
-  aRes, err := jsonpath.JsonPathLookup(aMap, aPath)
-  if err != nil {
-    return false, err
-  }
-  bRes, err := jsonpath.JsonPathLookup(bMap, bPath)
-  if err != nil {
-    return false, err
-  }
-  if op == EQ {
-    return reflect.DeepEqual(aRes,bRes), nil
-  }
-  if op == NE {
-    return !reflect.DeepEqual(aRes,bRes), nil
-  }
+	aRes, err := jsonpath.JsonPathLookup(aMap, aPath)
+	if err != nil {
+		return false, err
+	}
+	bRes, err := jsonpath.JsonPathLookup(bMap, bPath)
+	if err != nil {
+		return false, err
+	}
+	if op == EQ {
+		return reflect.DeepEqual(aRes, bRes), nil
+	}
+	if op == NE {
+		return !reflect.DeepEqual(aRes, bRes), nil
+	}
 
-  return false, nil
+	return false, nil
 }
-
-
--- jsonpath/jsonpath_test.go
+++ jsonpath/jsonpath_test.go
@@ -1,66 +1,64 @@
 package jsonpath
 
-
 import (
-  "testing"
-  "github.com/bmeg/arachne/protoutil"
+	"github.com/bmeg/arachne/protoutil"
+	"testing"
 )
 
-
 func TestCompare(t *testing.T) {
-  
-  //test structure
-  a := map[string]interface{}{
-    "a" : "hello",
-    "b" : 1,
-    "c" : true,
-    "d" : []interface{}{1,2,3},
-  }
-  // same as test structure, but decared seperately (to avoid comparing pointers)
-  b := map[string]interface{}{
-    "a" : "hello",
-    "b" : 1,
-    "c" : true,    
-    "d" : []interface{}{1,2,3},
-  }
-  // different data
-  c := map[string]interface{}{
-    "a" : "world",
-    "b" : 2,
-    "c" : false,    
-    "d" : []interface{}{4,5,6},
-  }
 
-  aStruct := protoutil.AsStruct(a)
-  bStruct := protoutil.AsStruct(b)
-  cStruct := protoutil.AsStruct(c)
+	//test structure
+	a := map[string]interface{}{
+		"a": "hello",
+		"b": 1,
+		"c": true,
+		"d": []interface{}{1, 2, 3},
+	}
+	// same as test structure, but decared separately (to avoid comparing pointers)
+	b := map[string]interface{}{
+		"a": "hello",
+		"b": 1,
+		"c": true,
+		"d": []interface{}{1, 2, 3},
+	}
+	// different data
+	c := map[string]interface{}{
+		"a": "world",
+		"b": 2,
+		"c": false,
+		"d": []interface{}{4, 5, 6},
+	}
 
-  if same, err := CompareFields(aStruct, bStruct, "$.a", "$.a", EQ); !same || err != nil {
-    t.Errorf("Fail $.a == $.a (%s)", err)
-  }
-  if same, err := CompareFields(aStruct, cStruct, "$.a", "$.a", EQ); same || err != nil {
-    t.Errorf("Fail $.a != $.a (%s)", err)
-  }
+	aStruct := protoutil.AsStruct(a)
+	bStruct := protoutil.AsStruct(b)
+	cStruct := protoutil.AsStruct(c)
 
-  if same, err := CompareFields(aStruct, bStruct, "$.b", "$.b", EQ); !same || err != nil {
-    t.Errorf("Fail $.b == $.b (%s)", err)
-  }
-  if same, err := CompareFields(aStruct, cStruct, "$.b", "$.b", EQ); same || err != nil {
-    t.Errorf("Fail $.b != $.b (%s)", err)
-  }
+	if same, err := CompareFields(aStruct, bStruct, "$.a", "$.a", EQ); !same || err != nil {
+		t.Errorf("Fail $.a == $.a (%s)", err)
+	}
+	if same, err := CompareFields(aStruct, cStruct, "$.a", "$.a", EQ); same || err != nil {
+		t.Errorf("Fail $.a != $.a (%s)", err)
+	}
 
-  if same, err := CompareFields(aStruct, bStruct, "$.c", "$.c", EQ); !same || err != nil {
-    t.Errorf("Fail $.c == $.c (%s)", err)
-  }
-  if same, err := CompareFields(aStruct, cStruct, "$.c", "$.c", EQ); same || err != nil {
-    t.Errorf("Fail $.c != $.c (%s)", err)
-  }
+	if same, err := CompareFields(aStruct, bStruct, "$.b", "$.b", EQ); !same || err != nil {
+		t.Errorf("Fail $.b == $.b (%s)", err)
+	}
+	if same, err := CompareFields(aStruct, cStruct, "$.b", "$.b", EQ); same || err != nil {
+		t.Errorf("Fail $.b != $.b (%s)", err)
+	}
 
-  if same, err := CompareFields(aStruct, bStruct, "$.d", "$.d", EQ); !same || err != nil {
-    t.Errorf("Fail $.d == $.d (%s)", err)
-  }
-  if same, err := CompareFields(aStruct, cStruct, "$.d", "$.d", EQ); same || err != nil {
-    t.Errorf("Fail $.d != $.d (%s)", err)
-  }
+	if same, err := CompareFields(aStruct, bStruct, "$.c", "$.c", EQ); !same || err != nil {
+		t.Errorf("Fail $.c == $.c (%s)", err)
+	}
+	if same, err := CompareFields(aStruct, cStruct, "$.c", "$.c", EQ); same || err != nil {
+		t.Errorf("Fail $.c != $.c (%s)", err)
+	}
+
+	if same, err := CompareFields(aStruct, bStruct, "$.d", "$.d", EQ); !same || err != nil {
+		t.Errorf("Fail $.d == $.d (%s)", err)
+	}
+	if same, err := CompareFields(aStruct, cStruct, "$.d", "$.d", EQ); same || err != nil {
+		t.Errorf("Fail $.d != $.d (%s)", err)
+	}
 
 }
