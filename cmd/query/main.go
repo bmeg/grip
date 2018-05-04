c0eb1a5de29217e9bc06bb2379a98e9d3711c88a
adamstruck <strucka@ohsu.edu>
2018-05-04 11:43:30 -0700

    cmd/query: updated javascript code to match python code

--- cmd/query/main.go
+++ cmd/query/main.go
@@ -3,7 +3,7 @@ package query
 import (
 	"encoding/json"
 	"fmt"
-	"log"
+	//"log"
 	"strings"
 
 	"github.com/bmeg/arachne/aql"
@@ -13,56 +13,54 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var ophionString = `
-
+var aqlString = `
 function query() {
-  function process(l) {
-   if (!l) {
-     l = []
-   } else if (_.isString(l)) {
-     l = [l]
-   } else if (!_.isArray(l)) {
-     console.log("not something we know how to make labels out of:")
-     console.log(l)
-   }
-   return l
-  }
+	function process(l) {
+		if (!l) {
+			l = []
+		} else if (_.isString(l)) {
+			l = [l]
+		} else if (!_.isArray(l)) {
+			throw "not something we know how to make labels out of"
+		}
+		return l
+	}
 
-  return {
-    query: [],
-    V: function(id) {
-      this.query.push({'v': process(id)})
-      return this
-    },
-    E: function(id) {
-      this.query.push({'e': process(id)})
-      return this
-    },
-    outgoing: function(label) {
-      this.query.push({'out': process(label)})
-      return this
-    },
-    incoming: function(label) {
-      this.query.push({'in': process(label)})
-      return this
-    },
-    both: function(label) {
-      this.query.push({'both': process(label)})
-      return this
-    },
-    outgoingEdge: function(label) {
-      this.query.push({'out_edge': process(label)})
-      return this
-    },
-    incomingEdge: function(label) {
-      this.query.push({'in_edge': process(label)})
-      return this
-    },
-    bothEdge: function(label) {
-      this.query.push({'both_edge': process(label)})
-      return this
-    },
-		mark: function(name) {
+	return {
+		query: [],
+		V: function(id) {
+			this.query.push({'v': process(id)})
+			return this
+		},
+		E: function(id) {
+			this.query.push({'e': process(id)})
+			return this
+		},
+		out: function(label) {
+			this.query.push({'out': process(label)})
+			return this
+		},
+		in_: function(label) {
+			this.query.push({'in': process(label)})
+			return this
+		},
+		both: function(label) {
+			this.query.push({'both': process(label)})
+			return this
+		},
+		outE: function(label) {
+			this.query.push({'out_edge': process(label)})
+			return this
+		},
+		inE: function(label) {
+			this.query.push({'in_edge': process(label)})
+			return this
+		},
+		bothE: function(label) {
+			this.query.push({'both_edge': process(label)})
+			return this
+		},
+		as_: function(name) {
 			this.query.push({'as': name})
 			return this
 		},
@@ -70,38 +68,14 @@ function query() {
 			this.query.push({'select': {'labels': process(marks)}})
 			return this
 		},
-		hasId: function(id) {
-			this.query.push({'has_id': process(id)})
-			return this
-		},
-		hasLabel: function(label) {
-			this.query.push({'has_label': process(label)})
-			return this
-		},
-		has: function(key, val) {
-			this.query.push({'has': {'key': key, 'within': process(val)}})
-			return this
-		},
-		values: function(v) {
-			this.query.push({'values': {'labels': process(v)}})
-			return this
-		},
-    limit: function(n) {
-      this.query.push({'limit': n})
-      return this
-    },
-    range: function(begin, end) {
-      this.query.push({'begin': b, 'end': e})
-      return this
-    },
-		groupCount: function(field) {
-			this.query.push({'group_count': field})
+		limit: function(n) {
+			this.query.push({'limit': n})
 			return this
 		},
 		count: function() {
 			this.query.push({'count': ''})
 			return this
-	  },
+		},
 		distinct: function(val) {
 			this.query.push({'distinct': process(val)})
 			return this
@@ -109,12 +83,117 @@ function query() {
 		render: function(r) {
 			this.query.push({'render': r})
 			return this
+		},
+		where: function(expression) {
+			this.query.push({'where': expression})
+			return this
+		},
+		aggregate: function() {
+			this.query.push({'aggregate': {'aggregations': Array.prototype.slice.call(arguments)}})
+			return this
 		}
-  }
+	}
 }
 
+// Where operators
+function and_() {
+	return {'and': {'expressions': Array.prototype.slice.call(arguments)}}
+}
+
+function or_() {
+	return {'or': {'expressions': Array.prototype.slice.call(arguments)}}
+}
+
+function not_(expression) {
+	return {'not': expression}
+}
+
+function eq(key, value) {
+	return {'condition': {'key': key, 'value': value, 'condition': 'EQ'}}
+}
+
+function neq(key, value) {
+	return {'condition': {'key': key, 'value': value, 'condition': 'NEQ'}}
+}
+
+function gt(key, value) {
+	return {'condition': {'key': key, 'value': value, 'condition': 'GT'}}
+}
+
+function gte(key, value) {
+	return {'condition': {'key': key, 'value': value, 'condition': 'GTE'}}
+}
+
+function lt(key, value) {
+	return {'condition': {'key': key, 'value': value, 'condition': 'LT'}}
+}
+
+function lte(key, value) {
+	return {'condition': {'key': key, 'value': value, 'condition': 'LTE'}}
+}
+
+function in_(key, values) {
+	if (!values) {
+		values = []
+	} else if (!_.isObject(l) && !_.isArray(l)) {
+		values = [values]
+	}
+	return {'condition': {'key': key, 'value': values, 'condition': 'IN'}}
+}
+
+function contains(key, value) {
+	return {'condition': {'key': key, 'value': value, 'condition': 'CONTAINS'}}
+}
+
+// Aggregation builders
+function term(name, label, field, size) {
+	agg = {
+		"name": name,
+		"term": {"label": label, "field": field}
+	}
+	if (size) {
+		if (!_.isNumber(percents)) {
+			throw "size expected to be a number"
+		}
+		agg["term"]["size"] = size
+	}
+	return agg
+}
+
+function percentile(name, label, field, percents) {
+	if (!percents) {
+		percents = [1, 5, 25, 50, 75, 95, 99]
+	} else if (_.isNumber(percents)) {
+			percents = [percents]
+	} else if (!_.isArray(percents)) {
+		throw "percents expected to be an array of numbers"
+	}
+
+	return {
+		"name": name,
+		"percentile": {
+			"label": label, "field": field, "percents": percents
+		}
+	}
+}
+
+function histogram(name, label, field, interval) {
+	if (interval) {
+		if (!_.isNumber(interval)) {
+			throw "size expected to be a number"
+		}
+	}
+	return {
+		"name": name,
+		"histogram": {
+			"label": label, "field": field, "interval": interval
+		}
+	}
+}
+
+// base query object
 O = {
-    query : query
+	query : query
 }
 `
 
@@ -134,36 +213,36 @@ var Cmd = &cobra.Command{
 			return err
 		}
 
-		if _, err := vm.RunString(ophionString); err != nil {
-			log.Printf("Error: %s", err)
+		if _, err := vm.RunString(aqlString); err != nil {
 			return err
 		}
 
 		queryString := args[1]
 		val, err := vm.RunString(queryString)
 		if err != nil {
-			log.Printf("Error: %s", err)
 			return err
 		}
-		queryJSON, _ := json.Marshal(val)
-		query := aql.GraphQuery{}
 
+		queryJSON, err := json.Marshal(val)
+		if err != nil {
+			return err
+		}
+		// log.Printf("Query: %s\n", string(queryJSON))
+
+		query := aql.GraphQuery{}
 		err = jsonpb.Unmarshal(strings.NewReader(string(queryJSON)), &query)
 		if err != nil {
-			log.Printf("Error: %s", err)
 			return err
 		}
 		query.Graph = args[0]
 
 		conn, err := aql.Connect(host, true)
 		if err != nil {
-			log.Printf("Error: %s", err)
 			return err
 		}
 
 		res, err := conn.Traversal(&query)
 		if err != nil {
-			log.Printf("Error: %s", err)
 			return err
 		}
 
