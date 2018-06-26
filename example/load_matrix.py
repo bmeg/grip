3898e3d83ab715f0d88422cba4b4d5ed6263eeee
Kyle Ellrott <kellrott@gmail.com>
2018-06-26 15:30:19 -0700

    Fixing conflicts

--- example/load_matrix.py
+++ example/load_matrix.py
@@ -10,33 +10,74 @@ import math
 
 def load_matrix(args):
     conn = aql.Connection(args.server)
+    if args.db not in list(conn.listGraphs()):
+        conn.addGraph(args.db)
     O = conn.graph(args.db)
 
-    matrix = pandas.read_csv(args.input, sep="\t", index_col=0).transpose()
+    matrix = pandas.read_csv(args.input, sep=args.sep, index_col=0)
+    if args.transpose:
+        matrix = matrix.transpose()
 
-    for c in matrix.columns:
-        if list(O.query().V(c).count())[0]['count'] == 0:
-            O.addVertex(c, "Protein")
-
-    for name, row in matrix.iterrows():
-        src = "%s:%s" % (args.data_type, name)
-        print("Loading: %s" % (src))
-        data = {}
+    if args.connect:
+        #every row x col creates an edge with the weight value
         for c in matrix.columns:
-            v = row[c]
-            if not math.isnan(v):
-                data[c] = v
-        O.addVertex(name, "Sample")
-        O.addVertex(src, "Data:%s" % (args.data_type), data)
-        O.addEdge(name, src, "has")
+            cname = "%s%s" % (args.col_prefix, c)
+            if list(O.query().V(c).count())[0]['count'] == 0:
+                if args.debug:
+                    print("AddVertex %s %s" % (c, args.col_label))
+                else:
+                    O.addVertex(c, args.col_label)
+        for r in matrix.index:
+            rname = "%s%s" % (args.row_prefix, r)
+            if list(O.query().V(r).count())[0]['count'] == 0:
+                if args.debug:
+                    print("AddVertex %s %s" % (r, args.row_label))
+                else:
+                    O.addVertex(r, args.row_label)
+
+        for name, row in matrix.iterrows():
+            rname = "%s%s" % (args.row_prefix, name)
+            print("Loading: %s" % (rname))
+            b = O.bulkAdd()
+            for c in matrix.columns:
+                cname = "%s%s" % (args.col_prefix, c)
+                v = row[c]
+                if not math.isnan(v):
+                    if args.debug:
+                        print("AddEdge: %s %s %s %s" % (rname,cname,args.edge_label,{args.edge_prop:v}) )
+                    else:
+                        b.addEdge(rname, cname, args.edge_label, {args.edge_prop:v})
+            b.execute()
+    else:
+        for name, row in matrix.iterrows():
+            rname = "%s%s" % (args.row_prefix, name)
+            print("Loading: %s" % (rname))
+            data = {}
+            for c in matrix.columns:
+                v = row[c]
+                if not math.isnan(v):
+                    data[c] = v
+            if args.debug:
+                print("Add Vertex %s %s" % (rname, args.row_label))
+            else:
+                O.addVertex(rname, args.row_label, data)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("db")
     parser.add_argument("input")
+    parser.add_argument("--sep", default="\t")
     parser.add_argument("--server", default="http://localhost:8201")
-    parser.add_argument("--data-type", dest="data_type", default="Expression")
-    parser.add_argument("--db", required=True)
+    parser.add_argument("--row-label", dest="row_label", default="Row")
+    parser.add_argument("--row-prefix", default="")
+    parser.add_argument("-t", "--transpose", action="store_true", default=False)
+    parser.add_argument("--connect", action="store_true", default=False)
+    parser.add_argument("--col-label", dest="col_label", default="Col")
+    parser.add_argument("--col-prefix", default="")
+    parser.add_argument("--edge-label", dest="edge_label", default="weight")
+    parser.add_argument("--edge-prop", dest="edge_prop", default="w")
+    parser.add_argument("-d", dest="debug", action="store_true", default=False)
 
     args = parser.parse_args()
     load_matrix(args)
