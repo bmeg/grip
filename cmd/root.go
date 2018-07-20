0bd1392c1eb7e6df438a7b102feb8b8341d3c96e
adamstruck <strucka@ohsu.edu>
2018-07-20 14:19:36 -0700

    added schema command; updated graphql docs

--- cmd/root.go
+++ cmd/root.go
@@ -6,13 +6,13 @@ import (
 	"github.com/bmeg/arachne/cmd/create"
 	"github.com/bmeg/arachne/cmd/drop"
 	"github.com/bmeg/arachne/cmd/dump"
-	"github.com/bmeg/arachne/cmd/example"
 	"github.com/bmeg/arachne/cmd/info"
 	"github.com/bmeg/arachne/cmd/list"
 	"github.com/bmeg/arachne/cmd/load"
 	"github.com/bmeg/arachne/cmd/mongoload"
 	"github.com/bmeg/arachne/cmd/query"
 	"github.com/bmeg/arachne/cmd/rdf"
+	"github.com/bmeg/arachne/cmd/schema"
 	"github.com/bmeg/arachne/cmd/server"
 	"github.com/bmeg/arachne/cmd/stream"
 	"github.com/bmeg/arachne/cmd/version"
@@ -27,19 +27,19 @@ var RootCmd = &cobra.Command{
 }
 
 func init() {
-	RootCmd.AddCommand(server.Cmd)
-	RootCmd.AddCommand(rdf.Cmd)
+	RootCmd.AddCommand(create.Cmd)
+	RootCmd.AddCommand(drop.Cmd)
+	RootCmd.AddCommand(dump.Cmd)
+	RootCmd.AddCommand(genBashCompletionCmd)
+	RootCmd.AddCommand(info.Cmd)
+	RootCmd.AddCommand(list.Cmd)
 	RootCmd.AddCommand(load.Cmd)
 	RootCmd.AddCommand(mongoload.Cmd)
 	RootCmd.AddCommand(query.Cmd)
-	RootCmd.AddCommand(dump.Cmd)
+	RootCmd.AddCommand(rdf.Cmd)
+	RootCmd.AddCommand(schema.Cmd)
+	RootCmd.AddCommand(server.Cmd)
 	RootCmd.AddCommand(stream.Cmd)
-	RootCmd.AddCommand(create.Cmd)
-	RootCmd.AddCommand(drop.Cmd)
-	RootCmd.AddCommand(list.Cmd)
-	RootCmd.AddCommand(info.Cmd)
-	RootCmd.AddCommand(example.Cmd)
-	RootCmd.AddCommand(genBashCompletionCmd)
 	RootCmd.AddCommand(version.Cmd)
 }
 
