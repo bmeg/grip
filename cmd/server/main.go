83f3eef31b0d68b414dfb9daf050961e5a495e3d
adamstruck <strucka@ohsu.edu>
2018-11-15 15:39:11 -0800

    updated docs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -19,6 +19,7 @@ import (
 	"github.com/bmeg/grip/mongo"
 	"github.com/bmeg/grip/psql"
 	"github.com/bmeg/grip/server"
+	_ "github.com/go-sql-driver/mysql" //import so mysql will register as a sql driver
 	"github.com/imdario/mergo"
 	_ "github.com/lib/pq" // import so postgres will register as a sql driver
 	log "github.com/sirupsen/logrus"
