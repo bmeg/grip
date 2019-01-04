892babf28d37af86444242e4fd890e89b30c2624
adamstruck <strucka@ohsu.edu>
2019-01-04 15:43:26 -0800

    adding indexStartOptimize test cases

--- engine/core/optimizer_test.go
+++ engine/core/optimizer_test.go
@@ -5,15 +5,130 @@ import (
 	"testing"
 
 	"github.com/bmeg/grip/gdbi"
+	"github.com/bmeg/grip/gripql"
+	"github.com/davecgh/go-spew/spew"
 )
 
 func TestIndexStartOptimize(t *testing.T) {
-	original := []gdbi.Processor{}
-	expected := []gdbi.Processor{}
+	expected := []gdbi.Processor{
+		&LookupVerts{ids: []string{"1", "2", "3"}},
+		&LookupVertexAdjOut{},
+	}
+	original := []gdbi.Processor{
+		&LookupVerts{},
+		&HasID{ids: []string{"1", "2", "3"}},
+		&LookupVertexAdjOut{},
+	}
 	optimized := indexStartOptimize(original)
-	if reflect.DeepEqual(optimized, expected) {
-		t.Log("actual:", optimized)
-		t.Log("expected:", exected)
+	if !reflect.DeepEqual(optimized, expected) {
+		t.Log("actual", spew.Sdump(optimized))
+		t.Log("expected:", spew.Sdump(expected))
 		t.Error("indexStartOptimize returned an unexpected result")
 	}
+
+	expected = []gdbi.Processor{
+		&LookupVerts{ids: []string{"1", "2", "3"}},
+		&LookupVertexAdjOut{},
+	}
+	original = []gdbi.Processor{
+		&LookupVerts{},
+		&Has{stmt: gripql.Within("_gid", "1", "2", "3")},
+		&LookupVertexAdjOut{},
+	}
+	optimized = indexStartOptimize(original)
+	if !reflect.DeepEqual(optimized, expected) {
+		t.Log("actual", spew.Sdump(optimized))
+		t.Log("expected:", spew.Sdump(expected))
+		t.Error("indexStartOptimize returned an unexpected result")
+	}
+
+	// order shouldnt matter
+	expected = []gdbi.Processor{
+		&LookupVerts{ids: []string{"1", "2", "3"}},
+		&Has{stmt: gripql.Eq("$.data.foo", "bar")},
+		&LookupVertexAdjOut{},
+	}
+	original = []gdbi.Processor{
+		&LookupVerts{},
+		&Has{stmt: gripql.Eq("$.data.foo", "bar")},
+		&Has{stmt: gripql.Within("_gid", "1", "2", "3")},
+		&LookupVertexAdjOut{},
+	}
+	optimized = indexStartOptimize(original)
+	if !reflect.DeepEqual(optimized, expected) {
+		t.Log("actual", spew.Sdump(optimized))
+		t.Log("expected:", spew.Sdump(expected))
+		t.Error("indexStartOptimize returned an unexpected result")
+	}
+
+	expected = []gdbi.Processor{
+		&LookupVertsIndex{labels: []string{"foo", "bar"}},
+		&LookupVertexAdjOut{},
+	}
+	original = []gdbi.Processor{
+		&LookupVerts{},
+		&HasLabel{labels: []string{"foo", "bar"}},
+		&LookupVertexAdjOut{},
+	}
+	optimized = indexStartOptimize(original)
+	if !reflect.DeepEqual(optimized, expected) {
+		t.Log("actual", spew.Sdump(optimized))
+		t.Log("expected:", spew.Sdump(expected))
+		t.Error("indexStartOptimize returned an unexpected result")
+	}
+
+	expected = []gdbi.Processor{
+		&LookupVertsIndex{labels: []string{"foo", "bar"}},
+		&LookupVertexAdjOut{},
+	}
+	original = []gdbi.Processor{
+		&LookupVerts{},
+		&Has{stmt: gripql.Within("_label", "foo", "bar")},
+		&LookupVertexAdjOut{},
+	}
+	optimized = indexStartOptimize(original)
+	if !reflect.DeepEqual(optimized, expected) {
+		t.Log("actual", spew.Sdump(optimized))
+		t.Log("expected:", spew.Sdump(expected))
+		t.Error("indexStartOptimize returned an unexpected result")
+	}
+
+	expected = []gdbi.Processor{
+		&LookupVertsIndex{labels: []string{"foo", "bar"}},
+		&Has{stmt: gripql.Eq("$.data.foo", "bar")},
+		&LookupVertexAdjOut{},
+	}
+	original = []gdbi.Processor{
+		&LookupVerts{},
+		&Has{stmt: gripql.Eq("$.data.foo", "bar")},
+		&Has{stmt: gripql.Within("_label", "foo", "bar")},
+		&LookupVertexAdjOut{},
+	}
+	optimized = indexStartOptimize(original)
+	if !reflect.DeepEqual(optimized, expected) {
+		t.Log("actual", spew.Sdump(optimized))
+		t.Log("expected:", spew.Sdump(expected))
+		t.Error("indexStartOptimize returned an unexpected result")
+	}
+
+	expected = []gdbi.Processor{
+		&LookupVerts{ids: []string{"1", "2", "3"}},
+		&Has{stmt: gripql.Eq("$.data.foo", "bar")},
+		&Has{stmt: gripql.Within("_label", "foo", "bar")},
+		&LookupVertexAdjOut{},
+	}
+	original = []gdbi.Processor{
+		&LookupVerts{},
+		&Has{stmt: gripql.Eq("$.data.foo", "bar")},
+		&Has{stmt: gripql.Within("_label", "foo", "bar")},
+		&Has{stmt: gripql.Within("_gid", "1", "2", "3")},
+		&LookupVertexAdjOut{},
+	}
+	optimized = indexStartOptimize(original)
+	if !reflect.DeepEqual(optimized, expected) {
+		t.Log("actual", spew.Sdump(optimized))
+		t.Log("expected:", spew.Sdump(expected))
+		t.Error("indexStartOptimize returned an unexpected result")
+	}
+
 }
