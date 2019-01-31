0e59c8ac4661fde17629f17651e980c5b7fc31f9
adamstruck <strucka@ohsu.edu>
2019-01-31 15:28:21 -0800

    added list labels cmd to cli

--- cmd/list/main.go
+++ cmd/list/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/bmeg/grip/gripql"
 	"github.com/bmeg/grip/util/rpc"
+	"github.com/golang/protobuf/jsonpb"
 	"github.com/spf13/cobra"
 )
 
@@ -13,6 +14,11 @@ var host = "localhost:8202"
 // Cmd is the declaration of the command line
 var Cmd = &cobra.Command{
 	Use:   "list",
+	Short: "List operations",
+}
+
+var listGraphsCmd = &cobra.Command{
+	Use:   "graphs",
 	Short: "List graphs",
 	Long:  ``,
 	Args:  cobra.NoArgs,
@@ -34,7 +40,45 @@ var Cmd = &cobra.Command{
 	},
 }
 
+var listLabelsCmd = &cobra.Command{
+	Use:   "labels <graph>}",
+	Short: "List the vertex and edge labels in a graph",
+	Long:  ``,
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		graph := args[0]
+
+		conn, err := gripql.Connect(rpc.ConfigWithDefaults(host), true)
+		if err != nil {
+			return err
+		}
+
+		resp, err := conn.ListLabels(graph)
+		if err != nil {
+			return err
+		}
+		m := jsonpb.Marshaler{
+			EnumsAsInts:  false,
+			EmitDefaults: false,
+			Indent:       "  ",
+			OrigName:     false,
+		}
+		txt, err := m.MarshalToString(resp)
+		if err != nil {
+			return fmt.Errorf("failed to marshal ListLabels response: %v", err)
+		}
+		fmt.Printf("%s\n", txt)
+		return nil
+	},
+}
+
 func init() {
-	flags := Cmd.Flags()
-	flags.StringVar(&host, "host", host, "grip server url")
+	gflags := listGraphsCmd.Flags()
+	gflags.StringVar(&host, "host", host, "grip server url")
+
+	lflags := listLabelsCmd.Flags()
+	lflags.StringVar(&host, "host", host, "grip server url")
+
+	Cmd.AddCommand(listGraphsCmd)
+	Cmd.AddCommand(listLabelsCmd)
 }
