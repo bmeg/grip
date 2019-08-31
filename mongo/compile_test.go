0e18402842f315bf25e89e328d579ed3fa2f3142
Kyle Ellrott <kellrott@gmail.com>
2019-08-30 17:14:39 -0700

    Adding unit test to check json path namespacing

--- mongo/compile_test.go
+++ mongo/compile_test.go
@@ -1,10 +1,15 @@
 package mongo
 
 import (
+	"fmt"
 	"testing"
+	"strings"
 
+	"github.com/bmeg/grip/jsonpath"
 	"github.com/bmeg/grip/gripql"
 	"github.com/bmeg/grip/util"
+	"github.com/globalsign/mgo/bson"
+
 )
 
 func TestQuerySizeLimit(t *testing.T) {
@@ -23,4 +28,31 @@ func TestQuerySizeLimit(t *testing.T) {
 	if err == nil {
 		t.Error("expected an error on compile")
 	}
+}
+
+
+func TestDistinctPathing(t *testing.T) {
+
+	fields := []string{"$case._gid", "$compound._gid"}
+
+	match := bson.M{}
+	keys := bson.M{}
+
+	for _, f := range fields {
+		namespace := jsonpath.GetNamespace(f)
+		fmt.Printf("Namespace: %s\n", namespace)
+		f = jsonpath.GetJSONPath(f)
+		f = strings.TrimPrefix(f, "$.")
+		if f == "gid" {
+			f = "_id"
+		}
+		if namespace != jsonpath.Current {
+			f = fmt.Sprintf("marks.%s.%s", namespace, f)
+		}
+		match[f] = bson.M{"$exists": true}
+		k := strings.Replace(f, ".", "_", -1)
+		keys[k] = "$" + f
+	}
+	fmt.Printf("%s\n", match)
+	fmt.Printf("%s\n", keys)
 }
