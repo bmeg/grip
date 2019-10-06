e2ead1e6bd543f9fc55844c8ac470e49589bbb63
Kyle Ellrott <kellrott@gmail.com>
2019-10-06 15:38:57 -0700

    Adding a secondary set of graph access functions to the grids driver that don't bother loading name information

--- grids/graph_path.go
+++ grids/graph_path.go
@@ -48,12 +48,12 @@ func (ggraph *GridsGraph) RawGetVertexList(ctx context.Context) <-chan *RawDataE
         default:
         }
         keyValue := it.Key()
-        _, vid := VertexKeyParse(keyValue)
-
+        _, vkey := VertexKeyParse(keyValue)
+        lkey := ggraph.kdb.keyMap.GetVertexLabel(vkey)
         o <- &RawDataElement{
-          Gid: vid,
+          Gid: vkey,
+          Label: lkey,
         }
-
       }
       return nil
     })
@@ -62,26 +62,174 @@ func (ggraph *GridsGraph) RawGetVertexList(ctx context.Context) <-chan *RawDataE
 }
 
 func (ggraph *GridsGraph) RawGetEdgeList(ctx context.Context) <-chan *RawDataElement {
-  //TODO:
-  return nil
+  o := make(chan *RawDataElement, 100)
+  go func() {
+    defer close(o)
+    ggraph.kdb.graphkv.View(func(it kvi.KVIterator) error {
+      ePrefix := EdgeListPrefix(ggraph.graphKey)
+      for it.Seek(ePrefix); it.Valid() && bytes.HasPrefix(it.Key(), ePrefix); it.Next() {
+        select {
+        case <-ctx.Done():
+          return nil
+        default:
+        }
+        keyValue := it.Key()
+        _, ekey, srcvkey, dstvkey, lkey := EdgeKeyParse(keyValue)
+        o <- &RawDataElement{
+          Gid: ekey,
+          Label: lkey,
+          From: srcvkey,
+          To: dstvkey,
+        }
+      }
+      return nil
+    })
+  }()
+  return o
 }
 
 func (ggraph *GridsGraph) RawGetOutChannel(reqChan chan *RawElementLookup, load bool, edgeLabels []string) chan *RawElementLookup {
-  //TODO:
-  return nil
+  o := make(chan *RawElementLookup, 100)
+  edgeLabelKeys := make([]uint64, 0, len(edgeLabels))
+  for i := range edgeLabels {
+    el, ok := ggraph.kdb.keyMap.GetLabelKey(edgeLabels[i])
+    if ok {
+      edgeLabelKeys = append(edgeLabelKeys, el)
+    }
+  }
+  go func() {
+    defer close(o)
+    ggraph.kdb.graphkv.View(func(it kvi.KVIterator) error {
+      for req := range reqChan {
+        ePrefix := SrcEdgePrefix(ggraph.graphKey, req.ID)
+        for it.Seek(ePrefix); it.Valid() && bytes.HasPrefix(it.Key(), ePrefix); it.Next() {
+          keyValue := it.Key()
+          _, _, _, dstvkey, lkey := SrcEdgeKeyParse(keyValue)
+          if len(edgeLabels) == 0 || containsUint(edgeLabelKeys, lkey) {
+            dstlkey := ggraph.kdb.keyMap.GetVertexLabel(dstvkey)
+            o <- &RawElementLookup{
+              Data: &RawDataElement{
+                Gid: dstvkey,
+                Label: dstlkey,
+              },
+              ID: req.ID,
+              Ref: req.Ref,
+            }
+          }
+        }
+      }
+      return nil
+    })
+  }()
+  return o
 }
 
 func (ggraph *GridsGraph) RawGetInChannel(reqChan chan *RawElementLookup, load bool, edgeLabels []string) chan *RawElementLookup {
-  //TODO:
-  return nil
+  o := make(chan *RawElementLookup, 100)
+  edgeLabelKeys := make([]uint64, 0, len(edgeLabels))
+  for i := range edgeLabels {
+    el, ok := ggraph.kdb.keyMap.GetLabelKey(edgeLabels[i])
+    if ok {
+      edgeLabelKeys = append(edgeLabelKeys, el)
+    }
+  }
+  go func() {
+    defer close(o)
+    ggraph.kdb.graphkv.View(func(it kvi.KVIterator) error {
+      for req := range reqChan {
+        ePrefix := DstEdgePrefix(ggraph.graphKey, req.ID)
+        for it.Seek(ePrefix); it.Valid() && bytes.HasPrefix(it.Key(), ePrefix); it.Next() {
+          keyValue := it.Key()
+          _, _, srcvkey, _, lkey := DstEdgeKeyParse(keyValue)
+          if len(edgeLabels) == 0 || containsUint(edgeLabelKeys, lkey) {
+            srclkey := ggraph.kdb.keyMap.GetVertexLabel(srcvkey)
+            o <- &RawElementLookup{
+              Data: &RawDataElement{
+                Gid: srcvkey,
+                Label: srclkey,
+              },
+              ID: req.ID,
+              Ref: req.Ref,
+            }
+          }
+        }
+      }
+      return nil
+    })
+  }()
+  return o
 }
 
 func (ggraph *GridsGraph) RawGetOutEdgeChannel(reqChan chan *RawElementLookup, load bool, edgeLabels []string) chan *RawElementLookup {
-  //TODO:
-  return nil
+  o := make(chan *RawElementLookup, 100)
+  edgeLabelKeys := make([]uint64, 0, len(edgeLabels))
+  for i := range edgeLabels {
+    el, ok := ggraph.kdb.keyMap.GetLabelKey(edgeLabels[i])
+    if ok {
+      edgeLabelKeys = append(edgeLabelKeys, el)
+    }
+  }
+  go func() {
+    defer close(o)
+    ggraph.kdb.graphkv.View(func(it kvi.KVIterator) error {
+      for req := range reqChan {
+        ePrefix := SrcEdgePrefix(ggraph.graphKey, req.ID)
+        for it.Seek(ePrefix); it.Valid() && bytes.HasPrefix(it.Key(), ePrefix); it.Next() {
+          keyValue := it.Key()
+          _, ekey, srcvkey, dstvkey, lkey := SrcEdgeKeyParse(keyValue)
+          if len(edgeLabels) == 0 || containsUint(edgeLabelKeys, lkey) {
+            o <- &RawElementLookup{
+              Data: &RawDataElement{
+                Gid: ekey,
+                Label: lkey,
+                From: srcvkey,
+                To: dstvkey,
+              },
+              ID: req.ID,
+              Ref: req.Ref,
+            }
+          }
+        }
+      }
+      return nil
+    })
+  }()
+  return o
 }
 
 func (ggraph *GridsGraph) RawGetInEdgeChannel(reqChan chan *RawElementLookup, load bool, edgeLabels []string) chan *RawElementLookup {
-  //TODO:
-  return nil
+  o := make(chan *RawElementLookup, 100)
+  edgeLabelKeys := make([]uint64, 0, len(edgeLabels))
+  for i := range edgeLabels {
+    el, ok := ggraph.kdb.keyMap.GetLabelKey(edgeLabels[i])
+    if ok {
+      edgeLabelKeys = append(edgeLabelKeys, el)
+    }
+  }
+  go func() {
+    defer close(o)
+    ggraph.kdb.graphkv.View(func(it kvi.KVIterator) error {
+      for req := range reqChan {
+        ePrefix := DstEdgePrefix(ggraph.graphKey, req.ID)
+        for it.Seek(ePrefix); it.Valid() && bytes.HasPrefix(it.Key(), ePrefix); it.Next() {
+          keyValue := it.Key()
+          _, ekey, srcvkey, dstvkey, lkey := DstEdgeKeyParse(keyValue)
+          if len(edgeLabels) == 0 || containsUint(edgeLabelKeys, lkey) {
+            o <- &RawElementLookup{
+              Data: &RawDataElement{
+                Gid: ekey,
+                Label: lkey,
+                From: srcvkey,
+                To: dstvkey,
+              },
+              ID: req.ID,
+              Ref: req.Ref,
+            }
+          }
+        }
+      }
+      return nil
+    })
+  }()
+  return o
 }
