f948dd2cec17ada907bf98ecfd0c9d8d939c779e
Kyle Ellrott <kellrott@gmail.com>
2021-02-17 18:19:25 -0800

    Unit test now setting up graphql endpoint and sending request. There are issues with the schema that is sampled

--- test/graphql/graphql_query_test.go
+++ test/graphql/graphql_query_test.go
@@ -4,8 +4,32 @@ package graphql
 import (
   "fmt"
   "testing"
+  "context"
+  "github.com/machinebox/graphql"
 )
 
 func TestGraphQL(t *testing.T) {
   fmt.Printf("Running GraphQL testing\n")
+  url := fmt.Sprintf("http://%s/graphql/%s", GraphQLAddr, GraphName)
+  client := graphql.NewClient(url)
+
+  // make a request
+  req := graphql.NewRequest(`
+    query {
+      Human(id:"1000"){
+        name,
+        friend_to_Human{
+          name
+        }
+      }
+    }
+  `)
+
+  ctx := context.Background()
+  respData := map[string]interface{}{}
+  if err := client.Run(ctx, req, &respData); err != nil {
+    t.Error(err)
+  }
+  fmt.Printf("Response: %#v\n", respData  )
+
 }
--- test/graphql/main_test.go
+++ test/graphql/main_test.go
@@ -1,9 +1,12 @@
 package graphql
 
-
 import (
-  "flag"
+	"context"
+	"flag"
 	"fmt"
+	"log"
+	"net/http"
+  "time"
 	"os"
 	"sort"
 	"testing"
@@ -11,20 +14,19 @@ import (
 	"github.com/bmeg/grip/config"
 	"github.com/bmeg/grip/elastic"
 	"github.com/bmeg/grip/gdbi"
-  "github.com/bmeg/grip/server"
+	"github.com/bmeg/grip/graphql"
 	"github.com/bmeg/grip/grids"
 	"github.com/bmeg/grip/gripql"
-  "github.com/bmeg/grip/gripql/schema"
-  "github.com/bmeg/grip/graphql"
+	"github.com/bmeg/grip/gripql/schema"
 	"github.com/bmeg/grip/kvgraph"
 	_ "github.com/bmeg/grip/kvi/badgerdb" // import so badger will register itself
 	_ "github.com/bmeg/grip/kvi/boltdb"   // import so bolt will register itself
 	_ "github.com/bmeg/grip/kvi/leveldb"  // import so level will register itself
 	"github.com/bmeg/grip/mongo"
 	"github.com/bmeg/grip/psql"
+	"github.com/bmeg/grip/server"
 	"github.com/bmeg/grip/util"
 	_ "github.com/lib/pq" // import so postgres will register as a sql driver
-
 )
 
 var configFile string
@@ -34,6 +36,8 @@ var dbname string
 var vertices = []*gripql.Vertex{}
 var edges = []*gripql.Edge{}
 
+var GraphQLAddr = "localhost:9080"
+var GraphName = "test-graph"
 
 func setupGraph() error {
 	// sort edges/vertices and insert one at a time to ensure the same write order
@@ -61,124 +65,135 @@ func setupGraph() error {
 }
 
 func TestMain(m *testing.M) {
-  flag.StringVar(&configFile, "config", configFile, "config file to use for tests")
-  flag.Parse()
+	flag.StringVar(&configFile, "config", configFile, "config file to use for tests")
+	flag.Parse()
 
-  configFile = "../" + configFile
+	configFile = "../" + configFile
 
-  vertChan, err := util.StreamVerticesFromFile("../../conformance/graphs/swapi.vertices", 2)
-  if err != nil {
-    panic(err)
-  }
-  for v := range vertChan {
-    vertices = append(vertices, v)
-  }
-  edgeChan, err := util.StreamEdgesFromFile("../../conformance/graphs/swapi.edges", 2)
-  if err != nil {
-    panic(err)
-  }
-  for e := range edgeChan {
-    edges = append(edges, e)
-  }
-
-  var exit = 1
-
-  defer func() {
-    fmt.Println("tests exiting with code", exit)
-    os.Exit(exit)
-  }()
-
-  conf := config.DefaultConfig()
-  if configFile != "" {
-    err := config.ParseConfigFile(configFile, conf)
-    if err != nil {
-      fmt.Printf("error processing config file: %v", err)
-      return
-    }
-  }
-
-  config.TestifyConfig(conf)
-  fmt.Printf("Test config: %+v\n", conf)
-  dbconfig := conf.Drivers[conf.Default]
-
-  if dbconfig.Badger != nil {
-    gdb, err = kvgraph.NewKVGraphDB("badger", *dbconfig.Badger)
-    defer func() {
-      os.RemoveAll(*dbconfig.Badger)
-    }()
-  } else if dbconfig.Bolt != nil {
-    gdb, err = kvgraph.NewKVGraphDB("bolt", *dbconfig.Bolt)
-    defer func() {
-      os.RemoveAll(*dbconfig.Bolt)
-    }()
-  } else if dbconfig.Level != nil {
-    gdb, err = kvgraph.NewKVGraphDB("badger", *dbconfig.Level)
-    defer func() {
-      os.RemoveAll(*dbconfig.Level)
-    }()
-  } else if dbconfig.Grids != nil {
-    gdb, err = grids.NewGraphDB(*dbconfig.Grids)
-    defer func() {
-      os.RemoveAll(*dbconfig.Grids)
-    }()
-  } else if dbconfig.Elasticsearch != nil {
-    gdb, err = elastic.NewGraphDB(*dbconfig.Elasticsearch)
-  } else if dbconfig.MongoDB != nil {
-    gdb, err = mongo.NewGraphDB(*dbconfig.MongoDB)
-  } else if dbconfig.PSQL != nil {
-    gdb, err = psql.NewGraphDB(*dbconfig.PSQL)
-  } else {
-    err = fmt.Errorf("unknown database")
-  }
-
-  err = gdb.AddGraph("test-graph")
-  if err != nil {
-    fmt.Println("Error: failed to add graph:", err)
-    return
-  }
-
-  db, err = gdb.Graph("test-graph")
-  if err != nil {
-    fmt.Println("Error: failed to connect to graph:", err)
-    return
-  }
-
-  if dbname != "existing-sql" {
-    err = setupGraph()
-    if err != nil {
-      fmt.Println("Error: setting up graph:", err)
-      return
-    }
-  }
-
-  srv, err := server.NewGripServer(conf, "./", map[string]gdbi.GraphDB{conf.Default:gdb})
-  if err != nil {
-    fmt.Println("Error: failed to init server", err)
-    return
-  }
-
-  conn := gripql.WrapClient(gripql.NewQueryDirectClient(srv), gripql.NewEditDirectClient(srv))
-
-	sch, err := schema.ScanSchema(conn, "test-graph", 10, []string{})
+	vertChan, err := util.StreamVerticesFromFile("../../conformance/graphs/swapi.vertices", 2)
 	if err != nil {
-    fmt.Println("Error: failed to init server", err)
+		panic(err)
+	}
+	for v := range vertChan {
+		vertices = append(vertices, v)
+	}
+	edgeChan, err := util.StreamEdgesFromFile("../../conformance/graphs/swapi.edges", 2)
+	if err != nil {
+		panic(err)
+	}
+	for e := range edgeChan {
+		edges = append(edges, e)
+	}
+
+	var exit = 1
+
+	defer func() {
+		fmt.Println("tests exiting with code", exit)
+		os.Exit(exit)
+	}()
+
+	conf := config.DefaultConfig()
+	if configFile != "" {
+		err := config.ParseConfigFile(configFile, conf)
+		if err != nil {
+			fmt.Printf("error processing config file: %v", err)
+			return
+		}
+	}
+
+	config.TestifyConfig(conf)
+	fmt.Printf("Test config: %+v\n", conf)
+	dbconfig := conf.Drivers[conf.Default]
+
+	if dbconfig.Badger != nil {
+		gdb, err = kvgraph.NewKVGraphDB("badger", *dbconfig.Badger)
+		defer func() {
+			os.RemoveAll(*dbconfig.Badger)
+		}()
+	} else if dbconfig.Bolt != nil {
+		gdb, err = kvgraph.NewKVGraphDB("bolt", *dbconfig.Bolt)
+		defer func() {
+			os.RemoveAll(*dbconfig.Bolt)
+		}()
+	} else if dbconfig.Level != nil {
+		gdb, err = kvgraph.NewKVGraphDB("badger", *dbconfig.Level)
+		defer func() {
+			os.RemoveAll(*dbconfig.Level)
+		}()
+	} else if dbconfig.Grids != nil {
+		gdb, err = grids.NewGraphDB(*dbconfig.Grids)
+		defer func() {
+			os.RemoveAll(*dbconfig.Grids)
+		}()
+	} else if dbconfig.Elasticsearch != nil {
+		gdb, err = elastic.NewGraphDB(*dbconfig.Elasticsearch)
+	} else if dbconfig.MongoDB != nil {
+		gdb, err = mongo.NewGraphDB(*dbconfig.MongoDB)
+	} else if dbconfig.PSQL != nil {
+		gdb, err = psql.NewGraphDB(*dbconfig.PSQL)
+	} else {
+		err = fmt.Errorf("unknown database")
+	}
+
+	err = gdb.AddGraph(GraphName)
+	if err != nil {
+		fmt.Println("Error: failed to add graph:", err)
 		return
 	}
-  fmt.Printf("Adding Schema\n")
+
+	db, err = gdb.Graph(GraphName)
+	if err != nil {
+		fmt.Println("Error: failed to connect to graph:", err)
+		return
+	}
+
+	if dbname != "existing-sql" {
+		err = setupGraph()
+		if err != nil {
+			fmt.Println("Error: setting up graph:", err)
+			return
+		}
+	}
+
+	srv, err := server.NewGripServer(conf, "./", map[string]gdbi.GraphDB{conf.Default: gdb})
+	if err != nil {
+		fmt.Println("Error: failed to init server", err)
+		return
+	}
+
+	conn := gripql.WrapClient(gripql.NewQueryDirectClient(srv), gripql.NewEditDirectClient(srv))
+
+	sch, err := schema.ScanSchema(conn, GraphName, 10, []string{})
+	if err != nil {
+		fmt.Println("Error: failed to init server", err)
+		return
+	}
+	fmt.Printf("Adding Schema\n")
 	err = conn.AddSchema(sch)
-  if err != nil {
-    fmt.Printf("Error: failed to add schema %s\n", err)
-    return
-  }
+	if err != nil {
+		fmt.Printf("Error: failed to add schema %s\n", err)
+		return
+	}
+	gqlHandler, err := graphql.NewClientHTTPHandler(conn)
 
-  gqlHandler, err := graphql.NewClientHTTPHandler(conn)
+	httpServer := http.Server{Addr: GraphQLAddr, Handler: gqlHandler}
 
-  fmt.Printf("%s\n", gqlHandler)
-  fmt.Printf("%s\n", sch)
+	go func() {
+		if err := httpServer.ListenAndServe(); err != http.ErrServerClosed {
+			// Error starting or closing listener:
+			log.Fatalf("HTTP server ListenAndServe: %v", err)
+		}
+	}()
 
-  defer os.RemoveAll(conf.Server.WorkDir)
+	defer os.RemoveAll(conf.Server.WorkDir)
+  time.Sleep(time.Second)
 
-  // run tests
-  exit = m.Run()
+	// run tests
+	exit = m.Run()
+
+	if err := httpServer.Shutdown(context.Background()); err != nil {
+		// Error from closing listeners, or context timeout:
+		log.Printf("HTTP server Shutdown: %v", err)
+	}
 
 }
