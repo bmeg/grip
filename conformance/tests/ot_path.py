7e66ee2029199262fcf3b5c9888c43da9a492a44
Kyle Ellrott <kellrott@gmail.com>
2021-03-10 09:10:12 -0800

    Getting path to work under mongo driver

--- conformance/tests/ot_path.py
+++ conformance/tests/ot_path.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import
 
 
-def test_path_1(man):
+def test_path_out(man):
     errors = []
 
     G = man.setGraph("swapi")
@@ -10,8 +10,23 @@ def test_path_1(man):
     count = 0
     for res in G.query().V("Film:1").out().out().out().path():
         if res[0]['vertex'] != "Film:1":
-            errors.append("Wrong first step %s != %s" % (res[0], "Film:1"))
+            errors.append("Wrong first step %s != %s" % (res[0]['vertex'], "Film:1"))
         count += 1
     if count != 1814:
+        errors.append("Incorrect count returned %d != %d" % (count, 1814))
+    return errors
+
+def test_path_out(man):
+    errors = []
+
+    G = man.setGraph("swapi")
+
+    count = 0
+    for res in G.query().V("Film:1").in_().in_().path():
+        print(res)
+        if res[0]['vertex'] != "Film:1":
+            errors.append("Wrong first step %s != %s" % (res[0]['vertex'], "Film:1"))
+        count += 1
+    if count != 106:
         errors.append("Incorrect count returned %d != %d" % (count, 1814))
     return errors
