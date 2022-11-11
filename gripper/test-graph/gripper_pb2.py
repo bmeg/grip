411f609d2fa3682c348c5e8bf966c30e49e7b559
Kyle Ellrott <kellrott@gmail.com>
2022-11-11 14:59:47 -0800

    Updating protoc built files

--- gripper/test-graph/gripper_pb2.py
+++ gripper/test-graph/gripper_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=b'Z\034github.com/bmeg/grip/gripper',
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\rgripper.proto\x12\x07gripper\x1a\x1cgoogle/protobuf/struct.proto\"\x07\n\x05\x45mpty\"\x1a\n\nCollection\x12\x0c\n\x04name\x18\x01 \x01(\t\"\x13\n\x05RowID\x12\n\n\x02id\x18\x01 \x01(\t\"?\n\nRowRequest\x12\x12\n\ncollection\x18\x01 \x01(\t\x12\n\n\x02id\x18\x02 \x01(\t\x12\x11\n\trequestID\x18\x03 \x01(\x04\"@\n\x0c\x46ieldRequest\x12\x12\n\ncollection\x18\x01 \x01(\t\x12\r\n\x05\x66ield\x18\x02 \x01(\t\x12\r\n\x05value\x18\x03 \x01(\t\"K\n\x03Row\x12\n\n\x02id\x18\x01 \x01(\t\x12%\n\x04\x64\x61ta\x18\x02 \x01(\x0b\x32\x17.google.protobuf.Struct\x12\x11\n\trequestID\x18\x03 \x01(\x04\"\'\n\x0e\x43ollectionInfo\x12\x15\n\rsearch_fields\x18\x01 \x03(\t2\xd8\x02\n\nGRIPSource\x12\x37\n\x0eGetCollections\x12\x0e.gripper.Empty\x1a\x13.gripper.Collection0\x01\x12\x41\n\x11GetCollectionInfo\x12\x13.gripper.Collection\x1a\x17.gripper.CollectionInfo\x12/\n\x06GetIDs\x12\x13.gripper.Collection\x1a\x0e.gripper.RowID0\x01\x12.\n\x07GetRows\x12\x13.gripper.Collection\x1a\x0c.gripper.Row0\x01\x12\x34\n\x0bGetRowsByID\x12\x13.gripper.RowRequest\x1a\x0c.gripper.Row(\x01\x30\x01\x12\x37\n\x0eGetRowsByField\x12\x15.gripper.FieldRequest\x1a\x0c.gripper.Row0\x01\x42\x1eZ\x1cgithub.com/bmeg/grip/gripperb\x06proto3'
+  serialized_pb=b'\n\rgripper.proto\x12\x07gripper\x1a\x1cgoogle/protobuf/struct.proto\"\x07\n\x05\x45mpty\"\x1a\n\nCollection\x12\x0c\n\x04name\x18\x01 \x01(\t\"\x13\n\x05RowID\x12\n\n\x02id\x18\x01 \x01(\t\"?\n\nRowRequest\x12\x12\n\ncollection\x18\x01 \x01(\t\x12\n\n\x02id\x18\x02 \x01(\t\x12\x11\n\trequestID\x18\x03 \x01(\x04\"@\n\x0c\x46ieldRequest\x12\x12\n\ncollection\x18\x01 \x01(\t\x12\r\n\x05\x66ield\x18\x02 \x01(\t\x12\r\n\x05value\x18\x03 \x01(\t\"K\n\x03Row\x12\n\n\x02id\x18\x01 \x01(\t\x12%\n\x04\x64\x61ta\x18\x02 \x01(\x0b\x32\x17.google.protobuf.Struct\x12\x11\n\trequestID\x18\x03 \x01(\x04\"\x8f\x01\n\x0e\x43ollectionInfo\x12\x15\n\rsearch_fields\x18\x01 \x03(\t\x12\x36\n\x08link_map\x18\x02 \x03(\x0b\x32$.gripper.CollectionInfo.LinkMapEntry\x1a.\n\x0cLinkMapEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\x32\xd8\x02\n\nGRIPSource\x12\x37\n\x0eGetCollections\x12\x0e.gripper.Empty\x1a\x13.gripper.Collection0\x01\x12\x41\n\x11GetCollectionInfo\x12\x13.gripper.Collection\x1a\x17.gripper.CollectionInfo\x12/\n\x06GetIDs\x12\x13.gripper.Collection\x1a\x0e.gripper.RowID0\x01\x12.\n\x07GetRows\x12\x13.gripper.Collection\x1a\x0c.gripper.Row0\x01\x12\x34\n\x0bGetRowsByID\x12\x13.gripper.RowRequest\x1a\x0c.gripper.Row(\x01\x30\x01\x12\x37\n\x0eGetRowsByField\x12\x15.gripper.FieldRequest\x1a\x0c.gripper.Row0\x01\x42\x1eZ\x1cgithub.com/bmeg/grip/gripperb\x06proto3'
   ,
   dependencies=[google_dot_protobuf_dot_struct__pb2.DESCRIPTOR,])
 
@@ -254,6 +254,44 @@ _ROW = _descriptor.Descriptor(
 )
 
 
+_COLLECTIONINFO_LINKMAPENTRY = _descriptor.Descriptor(
+  name='LinkMapEntry',
+  full_name='gripper.CollectionInfo.LinkMapEntry',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  create_key=_descriptor._internal_create_key,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='key', full_name='gripper.CollectionInfo.LinkMapEntry.key', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='value', full_name='gripper.CollectionInfo.LinkMapEntry.value', index=1,
+      number=2, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=b'8\001',
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=420,
+  serialized_end=466,
+)
+
 _COLLECTIONINFO = _descriptor.Descriptor(
   name='CollectionInfo',
   full_name='gripper.CollectionInfo',
@@ -269,10 +307,17 @@ _COLLECTIONINFO = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='link_map', full_name='gripper.CollectionInfo.link_map', index=1,
+      number=2, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
   ],
   extensions=[
   ],
-  nested_types=[],
+  nested_types=[_COLLECTIONINFO_LINKMAPENTRY, ],
   enum_types=[
   ],
   serialized_options=None,
@@ -281,11 +326,13 @@ _COLLECTIONINFO = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=322,
-  serialized_end=361,
+  serialized_start=323,
+  serialized_end=466,
 )
 
 _ROW.fields_by_name['data'].message_type = google_dot_protobuf_dot_struct__pb2._STRUCT
+_COLLECTIONINFO_LINKMAPENTRY.containing_type = _COLLECTIONINFO
+_COLLECTIONINFO.fields_by_name['link_map'].message_type = _COLLECTIONINFO_LINKMAPENTRY
 DESCRIPTOR.message_types_by_name['Empty'] = _EMPTY
 DESCRIPTOR.message_types_by_name['Collection'] = _COLLECTION
 DESCRIPTOR.message_types_by_name['RowID'] = _ROWID
@@ -338,14 +385,23 @@ Row = _reflection.GeneratedProtocolMessageType('Row', (_message.Message,), {
 _sym_db.RegisterMessage(Row)
 
 CollectionInfo = _reflection.GeneratedProtocolMessageType('CollectionInfo', (_message.Message,), {
+
+  'LinkMapEntry' : _reflection.GeneratedProtocolMessageType('LinkMapEntry', (_message.Message,), {
+    'DESCRIPTOR' : _COLLECTIONINFO_LINKMAPENTRY,
+    '__module__' : 'gripper_pb2'
+    # @@protoc_insertion_point(class_scope:gripper.CollectionInfo.LinkMapEntry)
+    })
+  ,
   'DESCRIPTOR' : _COLLECTIONINFO,
   '__module__' : 'gripper_pb2'
   # @@protoc_insertion_point(class_scope:gripper.CollectionInfo)
   })
 _sym_db.RegisterMessage(CollectionInfo)
+_sym_db.RegisterMessage(CollectionInfo.LinkMapEntry)
 
 
 DESCRIPTOR._options = None
+_COLLECTIONINFO_LINKMAPENTRY._options = None
 
 _GRIPSOURCE = _descriptor.ServiceDescriptor(
   name='GRIPSource',
@@ -354,8 +410,8 @@ _GRIPSOURCE = _descriptor.ServiceDescriptor(
   index=0,
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_start=364,
-  serialized_end=708,
+  serialized_start=469,
+  serialized_end=813,
   methods=[
   _descriptor.MethodDescriptor(
     name='GetCollections',
