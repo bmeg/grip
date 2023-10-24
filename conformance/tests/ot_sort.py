59a2bfa2dfc0d63dad9e604b87eeb809d2dadf0f
Kyle Ellrott <kellrott@gmail.com>
2023-10-24 16:03:29 -0700

    Adding sort statements and tests

--- conformance/tests/ot_sort.py
+++ conformance/tests/ot_sort.py
@@ -8,12 +8,38 @@ def test_sort_name(man):
     G = man.setGraph("swapi")
 
     q = G.query().V().hasLabel("Character").sort( "name" )
-
     last = ""
     for row in q:
         #print(row)
         if row["data"]["name"] < last:
             errors.append("incorrect sort: %s < %s" % (row["data"]["name"], last))
         last = row["data"]["name"]
+    return errors
+
+
+
+def test_sort_units(man):
+    errors = []
+    G = man.setGraph("swapi")
+
+    q = G.query().V().hasLabel("Vehicle").sort( "max_atmosphering_speed" )
+    last = 0
+    for row in q:
+        #print(row)
+        value = row["data"]["max_atmosphering_speed"]
+        if value < last:
+            errors.append("incorrect sort: %s < %s" % (value, last))
+        last = value
+
+   
+    q = G.query().V().hasLabel("Vehicle").sort( "max_atmosphering_speed", decending=True )
+    last = 1000000000
+    for row in q:
+        print(row)
+        value = row["data"]["max_atmosphering_speed"]
+        if value > last:
+            errors.append("incorrect sort: %s > %s" % (value, last))
+        last = value
+
     return errors
 
