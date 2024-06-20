0b97c4b64050c48b545fb672a91d4e3562da6158
Kyle Ellrott <kellrott@gmail.com>
2024-06-20 16:21:29 -0700

    Switching pivot to key/value storage based system

--- engine/core/processors.go
+++ engine/core/processors.go
@@ -3,6 +3,7 @@ package core
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"math"
 	"reflect"
@@ -12,6 +13,7 @@ import (
 	"github.com/bmeg/grip/gdbi"
 	"github.com/bmeg/grip/gdbi/tpath"
 	"github.com/bmeg/grip/gripql"
+	"github.com/bmeg/grip/kvi"
 	"github.com/bmeg/grip/log"
 	"github.com/bmeg/grip/util/copy"
 	"github.com/influxdata/tdigest"
@@ -442,34 +444,56 @@ type Pivot struct {
 func (r *Pivot) Process(ctx context.Context, man gdbi.Manager, in gdbi.InPipe, out gdbi.OutPipe) context.Context {
 	go func() {
 		defer close(out)
-		pivotMap := map[string]map[string]any{}
-		fmt.Printf("Doing Pivot %#v\n", r.Stmt)
-		for t := range in {
-			if t.IsSignal() {
-				out <- t
-				continue
-			}
-			fmt.Printf("Checking %#v\n", t.GetCurrent())
-			id := gdbi.TravelerPathLookup(t, r.Stmt.Id)
-			if idStr, ok := id.(string); ok {
-				field := gdbi.TravelerPathLookup(t, r.Stmt.Field)
-				if fieldStr, ok := field.(string); ok {
-					value := gdbi.TravelerPathLookup(t, r.Stmt.Value)
-					if o, ok := pivotMap[idStr]; ok {
-						o[fieldStr] = value
-						pivotMap[idStr] = o
-					} else {
-						o := map[string]any{fieldStr: value}
-						pivotMap[idStr] = o
+		kv := man.GetTempKV()
+		kv.BulkWrite(func(bl kvi.KVBulkWrite) error {
+			for t := range in {
+				if t.IsSignal() {
+					out <- t
+					continue
+				}
+				//fmt.Printf("Checking %#v\n", t.GetCurrent())
+				id := gdbi.TravelerPathLookup(t, r.Stmt.Id)
+				if idStr, ok := id.(string); ok {
+					field := gdbi.TravelerPathLookup(t, r.Stmt.Field)
+					if fieldStr, ok := field.(string); ok {
+						value := gdbi.TravelerPathLookup(t, r.Stmt.Value)
+						if v, err := json.Marshal(value); err == nil {
+							key := bytes.Join([][]byte{[]byte(idStr), []byte(fieldStr)}, []byte{0})
+							bl.Set(key, v)
+						}
 					}
 				}
 			}
-		}
-		fmt.Printf("Finished Pivot: %#v\n", pivotMap)
-		for k, v := range pivotMap {
-			v["_id"] = k
-			out <- &gdbi.BaseTraveler{Render: v}
-		}
+			return nil
+		})
+		kv.View(func(it kvi.KVIterator) error {
+			it.Seek([]byte{0})
+			lastKey := ""
+			curDict := map[string]any{}
+			for it.Seek([]byte{0}); it.Valid(); it.Next() {
+				tmp := bytes.Split(it.Key(), []byte{0})
+				curKey := string(tmp[0])
+				curField := string(tmp[1])
+				if lastKey == "" {
+					lastKey = curKey
+				}
+				var curData any
+				value, _ := it.Value()
+				json.Unmarshal(value, &curData)
+				if lastKey != curKey {
+					out <- &gdbi.BaseTraveler{Render: curDict}
+					curDict = map[string]any{}
+					curDict[curField] = curData
+					lastKey = curKey
+				} else {
+					curDict[curField] = curData
+				}
+			}
+			if lastKey != "" {
+				out <- &gdbi.BaseTraveler{Render: curDict}
+			}
+			return nil
+		})
 	}()
 	return ctx
 }
