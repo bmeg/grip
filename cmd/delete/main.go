359a0320072f49072e53b24306a56a3a63c8d051
matthewpeterkort <matthewpeterkort@gmail.com>
2024-08-12 13:43:15 -0700

    add suggested args

--- cmd/delete/main.go
+++ cmd/delete/main.go
@@ -2,6 +2,7 @@ package delete
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
 
@@ -13,6 +14,10 @@ import (
 
 var host = "localhost:8202"
 var file string
+var edges []string
+var vertices []string
+var graph string
+var data Data
 
 type Data struct {
 	Graph    string   `json:"graph"`
@@ -22,41 +27,54 @@ type Data struct {
 
 // Cmd command line declaration
 var Cmd = &cobra.Command{
-	Use:   "delete <graph elem list file>",
-	Short: "bulk delete",
-	Long:  ``,
-	Args:  cobra.ExactArgs(1),
+	Use:   "delete <graph>",
+	Short: "Delete data from a graph",
+	Long: `JSON File Format: {
+	"graph": 'graph_name',
+	"edges":['list of edge ids'],
+	"vertices":['list of vertice ids']
+}
+
+comma delimited --edges or --vertices arguments are also supported ex:
+--edges="edge1,edge2"`,
+	Args: cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
+		if file == "" && edges == nil && vertices == nil {
+			return fmt.Errorf("no input file path or --edges or --vertices arg was provided")
+		}
+
 		conn, err := gripql.Connect(rpc.ConfigWithDefaults(host), true)
 		if err != nil {
 			return err
 		}
-		file = args[0]
-		if file == "" {
-			log.Errorln("No input file found")
+		graph = args[0]
+
+		if file != "" {
+			jsonFile, err := os.Open(file)
+			if err != nil {
+				log.Errorf("Failed to open file: %s", err)
+			}
+			defer jsonFile.Close()
+
+			// Read the JSON file
+			byteValue, err := ioutil.ReadAll(jsonFile)
+			if err != nil {
+				log.Errorf("Failed to read file: %s", err)
+			}
+
+			// Unmarshal the JSON into the Data struct
+			err = json.Unmarshal(byteValue, &data)
+			if err != nil {
+				log.Errorf("Failed to unmarshal JSON: %s", err)
+			}
+		} else if edges != nil || vertices != nil {
+			data.Edges = edges
+			data.Vertices = vertices
 		}
 
-		jsonFile, err := os.Open(file)
-		if err != nil {
-			log.Errorf("Failed to open file: %s", err)
-		}
-		defer jsonFile.Close()
-
-		// Read the JSON file
-		byteValue, err := ioutil.ReadAll(jsonFile)
-		if err != nil {
-			log.Errorf("Failed to read file: %s", err)
-		}
-
-		// Unmarshal the JSON into the Data struct
-		var data Data
-		err = json.Unmarshal(byteValue, &data)
-		if err != nil {
-			log.Errorf("Failed to unmarshal JSON: %s", err)
-		}
-
-		log.WithFields(log.Fields{"graph": data.Graph}).Info("deleting data")
-		conn.BulkDelete(&gripql.DeleteData{Graph: data.Graph, Vertices: data.Vertices, Edges: data.Edges})
+		log.WithFields(log.Fields{"graph": graph}).Info("deleting data")
+		log.Info("VALUE OF DATA: ", data.Edges, data.Vertices)
+		conn.BulkDelete(&gripql.DeleteData{Graph: graph, Vertices: data.Vertices, Edges: data.Edges})
 
 		return nil
 	},
@@ -65,5 +83,7 @@ var Cmd = &cobra.Command{
 func init() {
 	flags := Cmd.Flags()
 	flags.StringVar(&host, "host", host, "grip server url")
+	flags.StringSliceVar(&edges, "edges", edges, "grip edges list")
+	flags.StringSliceVar(&vertices, "vertices", vertices, "grip vertices list")
 	flags.StringVar(&file, "file", file, "file name")
 }
